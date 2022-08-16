89ce5ae41523b9be51637e3dc7f8df24bb7e8211
Dmitry Krayniy <krdima92@gmail.com>
2022-08-16 16:55:00 +0400

    Updated pipeline

--- clean.ps1
+++ clean.ps1
@@ -1,16 +1,19 @@
 #!/usr/bin/env pwsh
 
-# Get component data and set necessary variables
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
-$testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
+# Get component metadata and set necessary variables
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
+$testImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
+$docImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
 
 # Remove docker images
 docker rmi $testImage --force
+docker rmi $docImage --force
 docker rmi -f $(docker images -f "dangling=true" -q) # remove build container if build fails
 docker image prune --force
 
 # Remove existed containers
-docker ps -a | Select-String -Pattern "Exit" | foreach($_) { docker rm $_.ToString().Split(" ")[0] }
+$exitedContainers = docker ps -a | Select-String -Pattern "Exit"
+foreach($c in $exitedContainers) { docker rm $c.ToString().Split(" ")[0] }
 
 # Remove unused volumes
 docker volume rm -f $(docker volume ls -f "dangling=true")
